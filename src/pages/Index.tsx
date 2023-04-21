378ff5c7994057aaadd3843163d109025f0f394f
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2023-04-21 15:30:40 +0300

    Добавил вывод логина в приветствие

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import { AuthContext, IAuthContext } from "../context";
 
 const Index: FC = () => {
+    const { auth } = useContext<IAuthContext>(AuthContext);
+
     return (
         <div className="d-flex flex-column align-items-center flex-fill mt-5">
-            <h2>Здравствуйте!</h2>
+            <h2>Здравствуйте, {auth?.login}!</h2>
             <h2>Выберите в меню слева желаемое действие</h2>
         </div>
     );
