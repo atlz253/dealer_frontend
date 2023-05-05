c0c0e0d40972377b42c16ab70745c9f4ab82107c
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2023-05-05 23:14:07 +0300

    Реализовал трасформацию таблицы в карточки на малениких экранах

---
+++ src/components/Contracts/Contracts.tsx
@@ -0,0 +1,70 @@
+import IBaseContract from "audio_diler_common/interfaces/IBaseContract";
+import { FC } from "react";
+import { Table } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import styles from "./Contracts.module.css";
+
+interface IContractsProps {
+    contracts: IBaseContract[]
+}
+
+const Contracts: FC<IContractsProps> = ({ contracts }) => {
+    const navigate = useNavigate();
+
+    return (
+        <Table hover>
+            <thead className={styles.head}>
+                <tr>
+                    <th>Номер договора</th>
+                    <th>Продавец</th>
+                    <th>Покупатель</th>
+                    <th>Сумма</th>
+                    <th>Дата создания</th>
+                </tr>
+            </thead>
+            <tbody className={styles.body}>
+                {contracts.map((contract: IBaseContract) =>
+                    <tr
+                        key={contract.id}
+                        onClick={() => navigate(`/contracts/${contract.id}`)}
+                        className={styles.row}
+                        title={`Нажмите, чтобы перейти к просмотру договора №${contract.id}`}
+                    >
+                        <td
+                            cell-name="Номер договора"
+                            className={styles.cell}
+                        >
+                            {contract.id}
+                        </td>
+                        <td
+                            cell-name="Продавец"
+                            className={styles.cell}
+                        >
+                            {contract.sellerName}
+                        </td>
+                        <td
+                            cell-name="Покупатель"
+                            className={styles.cell}
+                        >
+                            {contract.buyerName}
+                        </td>
+                        <td
+                            cell-name="Сумма"
+                            className={styles.cell}
+                        >
+                            {contract.price}
+                        </td>
+                        <td
+                            cell-name="Дата создания"
+                            className={styles.cell}
+                        >
+                            {contract.created}
+                        </td>
+                    </tr>
+                )}
+            </tbody>
+        </Table>
+    );
+}
+
+export default Contracts;
--- src/pages/ContractsPage.tsx
+++ src/pages/ContractsPage.tsx
@@ -1,14 +1,11 @@
-import React, { FC, useContext, useEffect, useState } from 'react';
-import { Button, Form, Table } from 'react-bootstrap';
+import { FC, useEffect, useState } from 'react';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import API from '../api/API';
 import IBaseContract from 'audio_diler_common/interfaces/IBaseContract';
-import { Link, redirect, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import IconButton from "../components/IconButton";
-import { AuthContext, IAuthContext } from "../context";
-import IResponse from "audio_diler_common/interfaces/IResponse";
 import tryServerRequest from '../utils/tryServerRequest';
+import Contracts from '../components/Contracts/Contracts';
 
 const ContractsPage: FC = () => {
     const [contracts, setContracts] = useState<IBaseContract[]>([]);
@@ -36,33 +33,9 @@ const ContractsPage: FC = () => {
                     onClick={() => navigate("/contracts/new")}
                 />
             </div>
-            <Table hover>
-                <thead>
-                    <tr>
-                        <th>Номер договора</th>
-                        <th>Продавец</th>
-                        <th>Покупатель</th>
-                        <th>Сумма</th>
-                        <th>Дата создания</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {contracts.map((contract: IBaseContract) =>
-                        <tr
-                            key={contract.id} 
-                            onClick={() => navigate(`/contracts/${contract.id}`)}
-                            style={{ cursor: "pointer" }}
-                            title={`Нажмите, чтобы перейти к просмотру договора №${contract.id}`}
-                        >
-                            <td>{contract.id}</td>
-                            <td>{contract.sellerName}</td>
-                            <td>{contract.buyerName}</td>
-                            <td>{contract.price}</td>
-                            <td>{contract.created}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </Table>
+            <Contracts 
+                contracts={contracts}
+            />
         </div>
     );
 }
